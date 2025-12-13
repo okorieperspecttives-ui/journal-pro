697ca1186275b3f3a77e10ddc57c675992a5db02
ok-perspectives <okorieperspecttives@gmail.com>
2025-12-13 18:22:35 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-full">
+        <h1 className="text-white bg-black">Journal Pro</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
